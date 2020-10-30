d91f48ecdd3493613b4ef9be03bef737ce5df526
icung <saiful@mamiteam.com>
2020-10-31 00:16:14 +0700

    create simple alert

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,8 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
+  bool _alertIsVisible = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,11 +41,36 @@ class _GamePageState extends State<GamePage> {
                 'Hit Me',
                 style: TextStyle(color: Colors.blue),
               ),
-              onPressed: () {},
+              onPressed: () {
+                _alertIsVisible = true;
+                _showAler(context);
+                print("Button clicked!");
+              },
             )
           ],
         ),
       ),
     );
+  }
+
+  void _showAler(BuildContext context) {
+    Widget okButton = FlatButton(
+      child: Text("Awesome"),
+      onPressed: () {
+        Navigator.of(context).pop();
+        _alertIsVisible = false;
+        print("Awesome pressed! $_alertIsVisible");
+      },
+    );
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Hello ther"),
+            content: Text("This is my first pop-up"),
+            actions: <Widget>[okButton],
+            elevation: 5,
+          );
+        });
   }
 }

9aad5aeb7e6dba0b8c7f65bb8b400505999ed787
icung <saiful@mamiteam.com>
2020-11-04 20:46:55 +0700

    utilize type interface

--- lib/main.dart
+++ lib/main.dart
@@ -71,8 +71,8 @@ class _GamePageState extends State<GamePage> {
   }
 
   int _pointsForCurrentRound() {
-    int maximumScore = 100;
-    int difference = (_model.target - _model.current).abs();
+    var maximumScore = 100;
+    var difference = (_model.target - _model.current).abs();
     if (difference < 0) {
       difference *= -1;
     }
